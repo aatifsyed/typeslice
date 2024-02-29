1bc32c578d20eff801c2642d4e63cc4dd9d8f6be
Aatif Syed <aatifsyedyp@gmail.com>
2024-02-29 13:38:09 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{iter, marker::PhantomData};
 
 pub enum Byte<const BYTE: u8> {}
 
@@ -6,53 +6,62 @@ pub enum Empty {}
 
 enum Never {}
 
-pub struct Concat<H, T> {
+pub struct Concat<L, R> {
     _never: Never,
-    _phantom: PhantomData<fn() -> (H, T)>,
+    _phantom: PhantomData<fn() -> (L, R)>,
 }
 
 pub trait TypeVec {
     const LEN: usize;
-    fn put_byte<'a>(v: &'a mut &'a mut [u8]);
+    fn write_and(v: &mut [u8]) -> &mut [u8];
     fn to_vec() -> Vec<u8> {
         let mut v = vec![0; Self::LEN];
-        Self::put_byte(&mut &mut *v);
+        Self::write_and(&mut v);
         v
     }
-}
-
-impl<const BYTE: u8> TypeVec for Concat<Byte<BYTE>, Empty> {
-    const LEN: usize = 1;
-    fn put_byte<'a>(v: &'a mut &'a mut [u8]) {
-        v[0] = BYTE;
-        let (_, rest) = v.split_at_mut(Self::LEN);
-        *v = rest;
-    }
-}
-
-impl<const LEFT: u8, const RIGHT: u8> TypeVec for Concat<Byte<LEFT>, Byte<RIGHT>> {
-    const LEN: usize = 2;
-    fn put_byte<'a>(v: &'a mut &'a mut [u8]) {
-        v[0] = LEFT;
-        v[1] = RIGHT;
-        let (_, rest) = v.split_at_mut(Self::LEN);
-        *v = rest;
-    }
-}
-
-impl<T> TypeVec for Concat<T, Empty>
-where
-    T: TypeVec,
-{
-    const LEN: usize = T::LEN;
-    fn put_byte<'a>(v: &'a mut &'a mut [u8]) {
-        T::put_byte(v)
-    }
+    fn bytes() -> impl Iterator<Item = u8>;
 }
 
 impl TypeVec for Empty {
     const LEN: usize = 0;
-    fn put_byte(_: &mut &mut [u8]) {}
+
+    fn write_and(it: &mut [u8]) -> &mut [u8] {
+        it
+    }
+
+    fn bytes() -> impl Iterator<Item = u8> {
+        iter::empty()
+    }
+}
+
+impl<const BYTE: u8> TypeVec for Byte<BYTE> {
+    const LEN: usize = 1;
+
+    fn write_and(v: &mut [u8]) -> &mut [u8] {
+        v[0] = BYTE;
+        let (_, rest) = v.split_at_mut(Self::LEN);
+        rest
+    }
+    fn bytes() -> impl Iterator<Item = u8> {
+        iter::once(BYTE)
+    }
+}
+
+impl<L, R> TypeVec for Concat<L, R>
+where
+    L: TypeVec,
+    R: TypeVec,
+{
+    const LEN: usize = L::LEN + R::LEN;
+
+    fn write_and(v: &mut [u8]) -> &mut [u8] {
+        let rest = L::write_and(v);
+        R::write_and(rest)
+    }
+
+    fn bytes() -> impl Iterator<Item = u8> {
+        L::bytes().chain(R::bytes())
+    }
 }
 
 pub const fn equal_len<T: TypeVec>(b: &[u8]) -> bool {
@@ -76,7 +85,7 @@ mod tests {
             Concat::<Concat<Byte<b'A'>, Byte<b'B'>>, Empty>::to_vec(),
             b"AB"
         );
-        type ABC = Concat<Concat<Concat<Byte<b'A'>, Byte<b'B'>>, Byte<b'C'>>, Empty>;
-        assert_eq!(ABC::to_vec(), b"AB");
+        type Abc = Concat<Concat<Concat<Byte<b'A'>, Byte<b'B'>>, Byte<b'C'>>, Empty>;
+        assert_eq!(Abc::to_vec(), b"ABC");
     }
 }
